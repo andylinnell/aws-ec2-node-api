400228980d055b1cdf504b6e2dfe9373fa99b075
Andy Linnell <andylinnell.cafo@gmail.com>
2023-02-23 15:37:22 -0500

    craeted ec2 api on aws

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from 'cors';
+import 'dotenv/config'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.listen('5001', () => console.log('listening on port 5001'))
+
+app.get('/', (req,res) => res.send('api working'))
