32dc84c47626cf4b4ae0c053c724b82aeb4baeff
Andy Linnell <andylinnell.cafo@gmail.com>
2023-02-23 16:02:09 -0500

    second commit

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import express from "express";
 import cors from 'cors';
 import 'dotenv/config'
 
+
+
+
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 app.listen('5001', () => console.log('listening on port 5001'))
 
-app.get('/', (req,res) => res.send('api working'))
+app.get('/', (req,res) => res.json('api working'))
